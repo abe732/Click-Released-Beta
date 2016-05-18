e279fd9d9fc10f378368c7b3389c5d828cec3c7b
Adam Behrens <behrens.adam@gmail.com>
2016-05-17 19:52:05 -0700

    updated grunt and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,30 @@
-// module.exports = function(grunt) {
-// grunt.initConfig({
-// 	pkg: grunt.file.readJSON('package.json'),
-// 	nodemon: {
-// 	  dev: {
-// 	    script: 'server.js'
-// 	  }
-// 	},
-//     jshint: {
-//       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-//       options: {
-//         globals: {
-//           jQuery: true
-//         }
-//       }
-//     },
-//     watch: {
-//       files: ['<%= jshint.files %>'],
-//       tasks: ['jshint']
-//     }
-//   });
+module.exports = function(grunt) {
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	nodemon: {
+	  dev: {
+	    script: 'server.js'
+	  }
+	},
 
-//   grunt.loadNpmTasks('grunt-contrib-jshint');
-//   grunt.loadNpmTasks('grunt-contrib-watch');
+  // jshint: {
+  //   files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+  //   options: {
+  //     globals: {
+  //       jQuery: true
+  //     }
+  //   }
+  // },
 
-//   grunt.registerTask('default', ['jshint']);
+  // watch: {
+  //   files: ['<%= jshint.files %>'],
+  //   tasks: ['jshint']
+  // }
+  });
 
-// };
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  // grunt.registerTask('default', ['jshint']);
+
+};
